4b33baac5f90ee9eefb6f3dfd77de7e510a695bf
play4fun <play4fun@foxmail.com>
2021-07-04 12:21:45 +0800

    视频

--- 2021/6-19-list-groupby/README.md
+++ 2021/6-19-list-groupby/README.md
@@ -1,5 +1,9 @@
 
 # python list groupby
+- python 列表 排序+分组
+    - 需求： 按字段排序，然后分组
+- 视频 [【编程】一行代码！Python 列表 排序+分组 sorted groupby Counter](https://www.bilibili.com/video/BV1LM4y1g7Vu/)
+
 - 参考
     - [B站搜索](https://search.bilibili.com/all?keyword=python%20list%20groupby&from_source=web_search)
     - [Python list列表groupby分组用法](https://blog.csdn.net/xiaoc100200/article/details/111402566)
--- 2021/6-19-list-groupby/groupby.py
+++ 2021/6-19-list-groupby/groupby.py
@@ -2,11 +2,14 @@
 '''
 @File    :   groupby.py
 @Time    :   2021/06/19 09:38:40
-@Author  :   HG
-@Desc    :   
+@Author  :   GH
+@Desc    :   https://blog.csdn.net/xiaoc100200/article/details/111402566
+python 列表 排序+分组
+需求： 按字段排序，然后分组
 '''
 
 from itertools import groupby
+# from collections import Counter# 统计
 
 user_list = [
     {"uid": 1, "sex": "男", "age": 10},
@@ -15,13 +18,25 @@ user_list = [
     {"uid": 4, "sex": "女", "age": 31},
     {"uid": 2, "sex": "男", "age": 10}
 ]
-user_sort = sorted(user_list, key=lambda x: (x["sex"], x["age"]))
+user_sort = sorted(user_list, key=lambda x: (x["sex"], x["age"]))  # 必须排序
 [{'uid': 4, 'sex': '女', 'age': 20},
  {'uid': 4, 'sex': '女', 'age': 31},
  {'uid': 1, 'sex': '男', 'age': 10},
  {'uid': 2, 'sex': '男', 'age': 10},
  {'uid': 3, 'sex': '男', 'age': 20}]
 
+# 手写代码
+outD = {}
+for d in user_list:
+    tl = outD.get(d['sex'], [])
+    tl.append(d)
+    outD[d['sex']] = tl
+# print(outD)
+for k, v in outD.items():
+    print(k, ':', v)
+
+# 使用系统库
+# 一行代码
 user_group = groupby(user_sort, key=lambda x: (x["sex"], x["age"]))
 for key, group in user_group:
     print(key, list(group))
@@ -36,11 +51,11 @@ print('-'*30)
 
 def g(x):  # 自定义分组key
     if (x['age'] > 0) and (x['age'] <= 10):
-        return 'small'
+        return 'small'  # 儿童
     elif (x['age'] > 10) and (x['age'] <= 20):
-        return 'mid'
+        return 'mid'  # 少年
     else:
-        return 'max'
+        return 'max'  # 大人
 
 
 user_group = groupby(user_sort, key=g)
